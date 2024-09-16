565a74b9b689c11e36ad81f5c4b4cc644f9708f4
behzad05 <39502078+behzad05@users.noreply.github.com>
2024-09-16 16:29:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 alert("script is loaded from github 2");
-document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(14) > button").style.display = "none";
+(function () {
+    // Function to hide the target element when it appears
+    function hideTargetElement() {
+        const targetSelector = "#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(14) > button";
+        const targetElement = document.querySelector(targetSelector);
+
+        if (targetElement) {
+            targetElement.style.display = "none";
+            console.log("Element hidden successfully");
+        }
+    }
+
+    // Create a Mutation Observer to watch for changes in the DOM
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            hideTargetElement(); // Call the function whenever there's a change in the DOM
+        });
+    });
+
+    // Start observing the body for changes in child elements
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+
+    // Run the function once initially in case the element is already present
+    hideTargetElement();
+})();
