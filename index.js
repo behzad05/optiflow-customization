d34b4b174d01587a2019d39a3b216a8fe30c7b25
behzad05 <39502078+behzad05@users.noreply.github.com>
2024-09-16 16:21:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
+alert("script is loaded from github 2")
 document.querySelector("#smartlists > div.hl_controls.hl_smartlists--controls > div.hl_controls--left > span.bulk-actions-list > span:nth-child(14) > button").style.display = "none";
